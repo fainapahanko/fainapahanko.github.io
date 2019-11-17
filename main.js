1f58a6b14ba87b43c83d3ffa35710d32f4f44004
Faina Poganko <fainapoganko@MacBook-Air-Faina.local>
2019-11-17 11:50:28 +0100

    collapse problem solved

--- main.js
+++ main.js
@@ -35,22 +35,23 @@ loadFilmsBackOffice = async() => {
     if(events.length > 0){
         events.forEach(async event => {
             let films = await getEvent(event)
+            console.log(films)
             divWithFilms.innerHTML += films.map(film => 
                 `
                 <div class="card" id="div${film._id}">
-                <a href="backoffice.html?id=${film._id}&name=${film.name}" class="card-a"><h6 class="card-a">Go to films page</6></a>
+                <a href="backoffice.html?id=${film._id}&name=${film.name}" class="card-a"><h6 class="card-a">Go to films page</h6></a>
                     <div>
                         <i class="fas fa-times-circle delete-btn" onclick="deleteFilm('${film._id}')"></i>
                         <a href="backoffice.html?id=${film.name}"><i onclick="updateFilm('${film.category}')" class="fas fa-edit update-btn"></i></a>
                     </div>
-                    <div class="card-header" id="${film.name}">
-                        <h5 class="mb-0">
-                            <button class="btn btn-link btn-backoffice collapsed" data-toggle="collapse" data-target="#${film._id}" aria-expanded="false" aria-controls="${film._id}">
+                    <div class="card-header" id="heading${film._id}">
+                        <h5 class="mb-0 name-of-film-backoffice">
+                            <button class="btn btn-link btn-backoffice collapsed" data-toggle="collapse" data-target="#collapse${film._id}" aria-expanded="false" aria-controls="collapse${film._id}">
                             ${film.name} 
                             </button>
                         </h5>
                     </div>
-                    <div id="${film._id}" class="collapse" aria-labelledby="${film.name}" data-parent="#accordion">
+                    <div id="collapse${film._id}" class="collapse" aria-labelledby="heading${film._id}" data-parent="#accordion">
                         <div class="card-body row">
                             <div class="col-4">
                                 <img src="${film.imageUrl}" alt="" class="film-image">
