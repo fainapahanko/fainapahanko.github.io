ccc33f997b4f9865f491f0ef3e48451a65a30fa1
Faina Poganko <fainapoganko@MacBook-Air-Faina.local>
2019-11-17 13:40:36 +0100

    details page done

--- main.js
+++ main.js
@@ -39,13 +39,8 @@ loadFilmsBackOffice = async() => {
             divWithFilms.innerHTML += films.map(film => 
                 `
                 <div class="card" id="div${film._id}">
-                <a href="backoffice.html?id=${film._id}&name=${film.name}" class="card-a"><h6 class="card-a">Go to films page</h6></a>
-                    <div>
-                        <i class="fas fa-times-circle delete-btn" onclick="deleteFilm('${film._id}')"></i>
-                        <a href="backoffice.html?id=${film.name}"><i onclick="updateFilm('${film.category}')" class="fas fa-edit update-btn"></i></a>
-                    </div>
                     <div class="card-header" id="heading${film._id}">
-                        <h5 class="mb-0 name-of-film-backoffice">
+                        <h5 class="mb-0">
                             <button class="btn btn-link btn-backoffice collapsed" data-toggle="collapse" data-target="#collapse${film._id}" aria-expanded="false" aria-controls="collapse${film._id}">
                             ${film.name} 
                             </button>
@@ -61,6 +56,12 @@ loadFilmsBackOffice = async() => {
                                     <li>${film.name}</li>
                                     <li>${film.category}</li>
                                 </ul>
+                                <div>
+                                    <a href="details.html?id=${film._id}&name=${film.name}&category=${film.category}" target="_blank"><button class="btn btn-info my-1">Update film</button></a>
+                                </div>
+                                <div>
+                                    <button class="btn btn-danger my-1" onclick="deleteFilm('${film._id}')">Delete film</button>
+                                </div>
                             </div>
                             <div class="col-4">
                                 <p>${film.description}</p>
@@ -76,6 +77,50 @@ loadFilmsBackOffice = async() => {
         <h2>No films</h2>
         `
     }
+}
+
+loadFilmDetails = async (id, category) => {
+    let events = await getEvent(category)
+    const div = document.querySelector("#detailsDiv")
+    console.log("event", events)
+    console.log("event id", events._id)
+    console.log("id", id)
+    let film = events.filter(event => event._id == id)
+    console.log(film)
+    div.innerHTML += `
+    <div class="col-12 col-sm-6 p-5">
+        <h2>${film[0].name}</h2>
+        <div>
+            <img src="${film[0].imageUrl}" alt="">
+        </div>
+    </div>
+    <div class="col-12 col-sm-6 p-5">
+        <form style="width:500px" onsubmit="handleSubmit()">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Name</label>
+                <input type="text" class="form-control" id="name" placeholder="${film[0].name}" required>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Description</label>
+                <input type="text" class="form-control" id="description" style="height:70px; border:1px solid white" placeholder="${film[0].description}" required>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Category</label>
+                <input type="text" class="form-control" id="category" placeholder="${film[0].category}" required>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">ImageUrl</label>
+                <input type="text" class="form-control" id="imageUrl" placeholder="update image" required>
+            </div>
+            <input
+                id="submitBtn"
+                type="submit"
+                class="btn btn-primary"
+                value="UPDATE"
+            />
+            </form>
+        </div>
+    `
 }
 
 postEvent = async event => {
